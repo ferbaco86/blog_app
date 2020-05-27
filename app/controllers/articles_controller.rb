fb0254a27c19d91338a820f2e2ee89fc8638ec67
Fernando Bahamondes <ferbaco@gmail.com>
2020-05-27 18:32:17 -0400

    Add an update method and view. Add flash notices on the views and controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-
+    
+    flash.notice =  "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
@@ -24,6 +25,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
+    flash.notice =  "Article '#{@article.title}' Destroyed!"
     redirect_to articles_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice =  "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
   end
 end
