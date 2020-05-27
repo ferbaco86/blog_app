24da65df139795c8bcc22ebac3900a12c878674e
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-27 16:30:39 -0500

    Add create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,18 @@ class ArticlesController < ApplicationController
   end
   
   def show
-     @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new
-     @article = Article.new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+    redirect_to article_path(@article)
   end
 end
