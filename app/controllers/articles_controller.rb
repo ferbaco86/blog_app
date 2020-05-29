01572a3912525a2790817458ad959b75ee596d8e
Fernando Bahamondes <ferbaco@gmail.com>
2020-05-29 13:33:00 -0400

    Fix rubocop offenses

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
     @comment = Comment.new
@@ -18,8 +18,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    
-    flash.notice =  "Article '#{@article.title}' Created!"
+
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    flash.notice =  "Article '#{@article.title}' Destroyed!"
+    flash.notice = "Article '#{@article.title}' Destroyed!"
     redirect_to articles_path(@article)
   end
 
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(article_params)
 
-    flash.notice =  "Article '#{@article.title}' Updated!"
+    flash.notice = "Article '#{@article.title}' Updated!"
     redirect_to article_path(@article)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   root to: 'articles#index'
   resources :articles do
     resources :comments
   end
   resources :tags
-
 end
