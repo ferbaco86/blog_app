a099ff4deb663e792e29283dab2b9805305478cf
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-29 09:46:14 -0500

    Add delete link for tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles
 
+  root to: 'articles#index'
   resources :articles do
     resources :comments
   end
+  resources :tags
+
 end
